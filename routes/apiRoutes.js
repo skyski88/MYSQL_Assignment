31a0020fd4ea9de71fcaadd1b3d9ebb0e9717b16
Bradley Williams <skyski88@gmail.com>
2020-11-09 15:53:05 -0500

    updated

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+//Importing Dependencies
+const router = require ("express").Router();
+const store = require ("../db/store")
+
+//GET Route
+router.get("/notes", function(req, res){
+    store
+    .getNotes()
+    .then(notes=> res.json(notes))
+    .catch(err=> res.status(500).json(err));
+});
+
+//POST Route finish this section with tutor???
+router.post("/notes", (req, res) => {
+    store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err))
+});
+
+
+//DELETE Route
+router.delete("/notes/:id", function(req, res){
+    store
+    .removeNote(req.params.id)
+    .then(() => res.json({ok:true}))
+    .catch(err=> res.status(500).json(err));
+});
+
+
+module.exports = router;
